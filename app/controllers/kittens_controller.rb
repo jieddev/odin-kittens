eb317b90ec84cb9bdd1fb2b62aa8ea11aa8994c7
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-04 15:15:34 +0800

    Refactor kittens_controller.rb create action to create a new kitten through the new.html.erb

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -16,6 +16,23 @@ class KittensController < ApplicationController
   
   end
 
+  def update
+  end
+
+  def create 
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save 
+      redirect_to @kitten 
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+    def kitten_params 
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
 
 
 
